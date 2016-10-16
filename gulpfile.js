70a16ce29f6d2037a537f115e47eb17fd0b4526b
feizheng <1290657123@qq.com>
2016-10-16 16:16:17 +0800

    add sass for self

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,31 @@
   var autoprefixer = require('gulp-autoprefixer');
   var debug = require('gulp-debug');
   var rename = require('gulp-rename');
+  var concat = require('gulp-concat');
+  var files=[
+    'src/_variables.scss',
+    'src/_mixins.scss',
+    'src/_grid.scss',
+    'src/webkit-css-flex-grid.scss'
+  ];
 
 
   gulp.task('clean', function () {
     return del('dist');
   });
 
-  gulp.task('sass', function () {
-    return gulp.src('src/webkit-css-flex-grid.scss')
+  gulp.task('styles', function () {
+    return gulp.src(files)
+      .pipe(concat('webkit-css-flex-grid.scss'))
+      .pipe(gulp.dest('dist'))
       .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
       .pipe(gulp.dest('dist'))
-        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-        .pipe(rename({extname:'.min.css'}))
-        .pipe(gulp.dest('dist'));
+      .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+      .pipe(rename({extname:'.min.css'}))
+      .pipe(gulp.dest('dist'));
   });
 
-  gulp.task('default', ['clean', 'sass']);
+  gulp.task('default', ['clean', 'styles']);
 
 
 }());
