057fb4eee9a8dcfa035a3e5f4e5826372bbb2ca9
afeiship <1290657123@qq.com>
2019-11-28 11:51:55 +0800

    add: std + pkg publish

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,33 @@
-(function () {
-
-  var gulp = require('gulp');
-  var sass = require('gulp-sass');
-  var del = require('del');
-  var autoprefixer = require('gulp-autoprefixer');
-  var debug = require('gulp-debug');
-  var rename = require('gulp-rename');
-  var concat = require('gulp-concat');
-  var files=[
-    'src/_variables.scss',
-    'src/_mixins.scss',
-    'src/_grid.scss',
-    'src/webkit-sass-flexbox-grid.scss'
+(function() {
+  var gulp = require("gulp");
+  var sass = require("gulp-sass");
+  var del = require("del");
+  var autoprefixer = require("gulp-autoprefixer");
+  var debug = require("gulp-debug");
+  var rename = require("gulp-rename");
+  var concat = require("gulp-concat");
+  var files = [
+    "src/_variables.scss",
+    "src/_mixins.scss",
+    "src/_grid.scss",
+    "src/webkit-sass-flexbox-grid.scss"
   ];
 
-
-  gulp.task('clean', function () {
-    return del('dist');
+  gulp.task("clean", function() {
+    return del("dist");
   });
 
-  gulp.task('styles', function () {
-    return gulp.src(files)
-      .pipe(concat('webkit-sass-flexbox-grid.scss'))
-      .pipe(gulp.dest('dist'))
-      .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-      .pipe(gulp.dest('dist'))
-      .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-      .pipe(rename({extname:'.min.css'}))
-      .pipe(gulp.dest('dist'));
+  gulp.task("styles", function() {
+    return gulp
+      .src(files)
+      .pipe(concat("webkit-sass-flexbox-grid.scss"))
+      .pipe(gulp.dest("dist"))
+      .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+      .pipe(gulp.dest("dist"))
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      .pipe(rename({ extname: ".min.css" }))
+      .pipe(gulp.dest("dist"));
   });
 
-  gulp.task('default', ['clean', 'styles']);
-
-
-}());
+  gulp.task("default", gulp.series(["clean", "styles"]));
+})();
