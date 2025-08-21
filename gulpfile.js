d07fd3c963d0771e1a09d4beecb2147ddcf878c3
aric.zheng <1290657123@qq.com>
2025-08-21 13:09:22 +0800

    feat: upt new scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,29 @@
-(function () {
-  var gulp = require('gulp');
-  var sass = require('gulp-sass');
-  var del = require('del');
-  var path = require('path');
-  var pkgHeader = require('@jswork/gulp-pkg-header');
-  var SASS_INCLUDE_PATHS = [path.join(__dirname, '/node_modules/')];
-  var concat = require('gulp-concat');
-  var files = [
-    'src/_variables.scss',
-    'src/_mixins.scss',
-    'src/_grid.scss',
-    'src/index.scss',
-  ];
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const path = require('path');
+const pkgHeader = require('@jswork/gulp-pkg-header');
+const SASS_INCLUDE_PATHS = [path.join(__dirname, '/node_modules/')];
+const concat = require('gulp-concat');
+const files = [
+  'src/_variables.scss',
+  'src/_mixins.scss',
+  'src/_grid.scss',
+  'src/index.scss',
+];
 
-  gulp.task('clean', function () {
-    return del('dist');
-  });
+gulp.task('clean', function() {
+  return del('dist');
+});
 
-  gulp.task('styles', function () {
-    return gulp
-      .src(files)
-      .pipe(concat('index.scss'))
-      .pipe(pkgHeader())
-      .pipe(gulp.dest('dist'))
-      .pipe(sass({ outputStyle: 'expanded', includePaths: SASS_INCLUDE_PATHS }))
-      .pipe(gulp.dest('dist'));
-  });
+gulp.task('styles', function() {
+  return gulp
+    .src(files)
+    .pipe(concat('index.scss'))
+    .pipe(pkgHeader())
+    .pipe(gulp.dest('dist'))
+    .pipe(sass({ outputStyle: 'expanded', includePaths: SASS_INCLUDE_PATHS }))
+    .pipe(gulp.dest('dist'));
+});
 
-  gulp.task('default', gulp.series(['clean', 'styles']));
-})();
+gulp.task('default', gulp.series(['clean', 'styles']));
